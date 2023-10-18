3ba1d7c15cda6b44e6c958a35bf27c7b1d319668
George Halterman <113994972+Agoews@users.noreply.github.com>
2023-10-17 21:56:53 -0700

    Update streamlit_app.py
    breaking everything with 'from streamlit'

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,5 +43,5 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-my_cur.execute(f"INSERT INTO fruit_load_list (fruit_name) VALUES ('{add_my_fruit}')")
+my_cur.execute(f"INSERT INTO fruit_load_list (fruit_name) VALUES ('from streamlit')")
 streamlit.write('Thanks for adding ', add_my_fruit)

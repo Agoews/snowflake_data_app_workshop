e192ff0903c7baa8b22b0be89f71205819663da8
George Halterman <113994972+Agoews@users.noreply.github.com>
2023-10-17 22:11:03 -0700

    Update streamlit_app.py
    adding fn for query

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,11 @@ streamlit.dataframe(fruits_to_show)
 # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + 'kiwi')
 # streamlit.text(fruityvice_response.json())
 
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get('https://fruityvice.com/api/fruit/' + this_fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvoice_normalized
+
 streamlit.header('Fruityvice Fruit Advice!')
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
@@ -33,9 +38,8 @@ try:
     streamlit.error('Please select a fruit to get informaiton.')
   else:
     # Normalizes the data to be displayed  
-    fruityvice_response = requests.get('https://fruityvice.com/api/fruit/' + fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
 
 except URLError as e:
   streamlit.error()
